a6cc22349e2889c6e25f88c31719c35276db4cc7
bughou <applejava@qq.com>
2018-07-08 15:16:03 +0800

    update registry.sh cert location

--- registry.sh
+++ registry.sh
@@ -2,7 +2,12 @@
 
 if [ $# != 1 ]; then
   echo "  usage: $0 <domain-name>
-example: $0 registry.abc.com"
+example: $0 registry.abc.com
+https certificate location:
+  /etc/nginx/certs/\$cert.fullchain
+  /etc/nginx/certs/\$cert.key
+if \"\$cert\" env variable is empty, use <domain-name> without \"registry.\" prefix.
+"
   exit 1
 fi
 set -ex
@@ -16,6 +21,9 @@ main() {
 }
 
 setup_nginx() {
+  if test -z "$cert"; then
+    cert=${domain#registry.}
+  fi
   echo '## Set a variable to help us decide if we need to add the
 ## "Docker-Distribution-Api-Version" header.
 ## The registry always sets this header.
@@ -31,8 +39,8 @@ server {
   listen              443 ssl http2;
   ssl_protocols       TLSv1 TLSv1.1 TLSv1.2;
   ssl_ciphers         ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA:ECDHE-ECDSA-DES-CBC3-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:DES-CBC3-SHA:!DSS;
-  ssl_certificate     /etc/letsencrypt/live/$domain/fullchain.pem;
-  ssl_certificate_key /etc/letsencrypt/live/$domain/privkey.pem;
+  ssl_certificate     /etc/nginx/certs/$cert.fullchain;
+  ssl_certificate_key /etc/nginx/certs/$cert.key;
   ssl_session_cache   shared:SSL:10m;
   ssl_session_timeout 10m;
   access_log /var/log/nginx/$domain/access.log;
@@ -64,10 +72,6 @@ server {
     proxy_set_header  X-Forwarded-For   $proxy_add_x_forwarded_for;
     proxy_set_header  X-Forwarded-Proto $scheme;
     proxy_read_timeout                  900;
-  }
-
-  location /.well-known {
-    root /var/www/letsencrypt;
   }
 }' | sudo tee /etc/nginx/sites-enabled/$domain >/dev/null
   sudo mkdir -p /var/log/nginx/$domain
