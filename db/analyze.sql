25c5b6cf70ad0ae3f277770cd9731e4c15045df6
bughou <applejava@qq.com>
2018-10-20 12:21:17 +0800

    save

--- db/analyze.sql
+++ db/analyze.sql
@@ -1,6 +1,9 @@
 select relname, n_live_tup, n_dead_tup, case
     when n_live_tup > 0 then (n_dead_tup * 100 / n_live_tup)::text || '%'
   end as dead_ratio,
-  last_vacuum, last_autovacuum, last_analyze, last_autoanalyze
+  date(last_vacuum) as last_vacuum,
+  date(last_autovacuum) as last_autovacuum,
+  date(last_analyze) as last_analyze,
+  date(last_autoanalyze) as last_autoanalyze
 from pg_stat_user_tables
 where relname='company_parts' order by last_autoanalyze;
