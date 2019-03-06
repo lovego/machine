92eb4b76bdda37032c197fa84e7c50ab779eb11a
bughou <applejava@qq.com>
2019-03-06 16:27:50 +0800

    save

--- net.sh
+++ net.sh
@@ -1,2 +1,5 @@
 # 统计各个状态的连接数
-ss -n | awk '{ array[$2]++ } END { for (k in array) print k, array[k] }'
+ss -an | awk '{ array[$2]++ } END { for (k in array) print k, array[k] }' | sort -rnk2
+
+# 查看time-wait状态的连接
+ss -an state time-wait
