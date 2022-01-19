1dd97497b0a0eb33362c1a2391b268373cb4f35b
bughou <bughou@gmail.com>
2022-01-19 14:48:55 +0800

    update size.sql

--- db/size.sql
+++ db/size.sql
@@ -2,10 +2,10 @@ SELECT pg_database.datname, pg_size_pretty(pg_database_size(pg_database.datname)
 FROM pg_database
 ORDER by pg_database_size(pg_database.datname) DESC;
 
-SELECT table_schema, table_name, pg_size_pretty(pg_relation_size(
+SELECT table_schema, table_name, pg_size_pretty(pg_total_relation_size(
     '"' || table_schema || '"."' || table_name || '"'
 ))
 FROM information_schema.tables
-ORDER BY pg_relation_size('"' || table_schema || '"."' || table_name || '"') DESC;
+ORDER BY pg_total_relation_size('"' || table_schema || '"."' || table_name || '"') DESC;
 
 
